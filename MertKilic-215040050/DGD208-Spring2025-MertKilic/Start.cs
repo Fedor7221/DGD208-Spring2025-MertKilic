535d73a71aaee5acdef05989ecc817e9433b5411
Mert Kılıç 215040050 <mert36671@gmail.com>
2025-05-27 00:23:32 +0300

    Add files via upload

---
+++ MertKilic-215040050/DGD208-Spring2025-MertKilic/Start.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DGD208_Spring2025_MertKilic
+{
+    public class Start
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Start();
+        }
+    }
+}
