479234ec1b8a902a7637dd984bb6155355657b75
Mert Kılıç 215040050 <mert36671@gmail.com>
2025-06-03 16:17:09 +0300

    Update ItemDatabase.cs

--- MertKilic-215040050/DGD208-Spring2025-MertKilic/ItemDatabase.cs
+++ MertKilic-215040050/DGD208-Spring2025-MertKilic/ItemDatabase.cs
@@ -7,11 +7,11 @@ namespace DGD208_Spring2025_MertKilic
 {
     public static class ItemDatabase
 {
-    public static List<Item> items = new List<Item>
+    public static List<Item> items= new List<Item>
     {
-        new Item(ItemType.Food, 20, 2),
-        new Item(ItemType.Toy, 15, 1),
-        new Item(ItemType.Sleep, 25, 3)
+        new Item("food",ItemType. Food, 20, 2),
+        new Item("toy", ItemType.Toy, 15, 1),
+        new Item("sleep", ItemType.Sleep, 25 , 3)
     };
 }
 }
